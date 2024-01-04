f487dc0bb05095b12d60c59aeb7f6b8af6a009cf
adi surya <adisurya1@gmail.com>
2024-01-04 07:30:08 +0700

    add logger and ignore logs

---
+++ src/utils/logger.js
@@ -0,0 +1,27 @@
+const winston = require('winston');
+require('winston-daily-rotate-file');
+
+const transport = new winston.transports.DailyRotateFile({
+  filename: 'logs/application-%DATE%.log',
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+});
+const transportConsole = new winston.transports.Console();
+
+const logger = winston.createLogger({
+  level: 'silly',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    winston.format.json(),
+  ),
+  transports: [
+    transport,
+    transportConsole,
+  ],
+});
+
+module.exports = logger;
